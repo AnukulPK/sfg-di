d2c5a72dec6caa0d89853671ec53e855ca261990
Anukul <anukul.p@growthsourceft.com>
2021-11-22 16:37:45 +0530

    di added in various ways

--- src/main/java/com/anukul/sfgdi/SfgDiApplication.java
+++ src/main/java/com/anukul/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package com.anukul.sfgdi;
 
+import com.anukul.sfgdi.controllers.ConstructorInjectedController;
 import com.anukul.sfgdi.controllers.MyController;
+import com.anukul.sfgdi.controllers.PropertyInjectedController;
+import com.anukul.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,9 +16,24 @@ public class SfgDiApplication {
 
 		MyController myController = (MyController) ctx.getBean("myController");
 
-		String greeting = myController.sayHello();
+		System.out.println("-----------Primary Bean");
+		System.out.println(myController.sayHello());
 
-		System.out.println(greeting);
+		System.out.println("-----------Property");
+
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+
+		System.out.println("-----------Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+
+		System.out.println("--------------Constructor");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+
 	}
 
 }
